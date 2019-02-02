6ed64eeceb75a054303b92816d19e5b5a9c0ec32
Eeshani Patel <eeshani.p@keka.com>
2019-02-02 18:48:31 +0530

    main.html added

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,33 @@
 from django.shortcuts import render     # render html templates
 from django.http import HttpResponse    # handle request of user
 
+# Data = we have list of dictionary
+
+posts = [
+    {
+        'author': 'Eeshani Patel',
+        'title':' My first post',
+        'content': 'My first post description',
+        'date_posted': 'January 19, 2019', 
+    },
+    {
+        'author': 'John Callahan',
+        'title':' Hello World',
+        'content': 'This is Callahan',
+        'date_posted': 'February 1, 2018', 
+    },
+]
+
 # Create your views here.
 def home(request):
+    # will create a context,i.e dictionary, to pass the data to home template
+    context = {
+        'posts': posts,
+    }
     #will return an http response
-    return render(request, 'blog/home.html')        # args pass request, template name and data(context)
+    return render(request, 'blog/home.html', context)        # args pass request, template name and data(context)
 
 def about(request):
     #will return an http response
     #return HttpResponse('<h1>About View</h1>')
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title':'About'})
