954a1970578e87cf4ccf2bef61b2b5e85b0a449b
Eeshani Patel <eeshani.p01@gmail.com>
2019-02-03 00:50:35 +0530

    users app added

--- blog_django/urls.py
+++ blog_django/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include   #include from urls for including other urls file
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', user_views.register, name="register"),
     path('', include('blog.urls')),
 ]
