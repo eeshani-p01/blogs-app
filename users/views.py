206b16e4612fcb964facf4f88bc09a38a73d6f05
Eeshani Patel <eeshani.p01@gmail.com>
2019-02-03 01:00:42 +0530

    custom form added

--- users/views.py
+++ users/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, redirect   # to change the route
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages     #like toastr message.error, debug, info, warning, success
+from .forms import UserRegisterForm
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)   # using our custom defined form 
         if form.is_valid():
+            form.save()     # to add that to database
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('blogs-home')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
